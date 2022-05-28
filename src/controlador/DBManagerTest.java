94ac0f28273f2d8f86f29bc2bcd7017b7ea6a124
InmaB <inmabalbuena@gmail.com>
2022-05-28 13:11:19 +0200

    JavaUnit
    Intentando hacer pruebas con javaunit

---
+++ src/controlador/DBManagerTest.java
@@ -0,0 +1,83 @@
+package controlador;
+
+import junit.framework.TestCase;
+
+public class DBManagerTest extends TestCase {
+
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsConnected() {
+		fail("Not yet implemented");
+	}
+
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+	public void testMostrarBaseDatos() {
+		fail("Not yet implemented");
+	}
+
+	public void testUsarBBDD() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetTablaClientesIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetTablaClientes() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrintTablaClientes() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testExistsCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrintCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testInsertCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testOrdenarClientes() {
+		fail("Not yet implemented");
+	}
+
+	public void testVolcarTabla() {
+		fail("Not yet implemented");
+	}
+
+	public void testCrearTabla() {
+		fail("Not yet implemented");
+	}
+
+	public void testConseguirFila() {
+		fail("Not yet implemented");
+	}
+
+	public void testMostrarFila() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/controlador/GestionClientesTest.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import junit.framework.TestCase;
+
+public class GestionClientesTest extends TestCase {
+
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	public void testMenuPrincipal() {
+		fail("Not yet implemented");
+	}
+
+	public void testPideInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testOpcionMostrarClientes() {
+		fail("Not yet implemented");
+	}
+
+	public void testOpcionMostrarFila() {
+		fail("Not yet implemented");
+	}
+
+	public void testOpcionNuevoCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testOpcionModificarCliente() {
+		fail("Not yet implemented");
+	}
+
+	public void testOpcionEliminarCliente() {
+		fail("Not yet implemented");
+	}
+
+}
